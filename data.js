2cd11903fd87a9af9e624abc01ac7bb424a4d936
payakumbuhpedia <payakumbuhpediastoreid@gmail.com>
2026-04-01 02:24:31 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,43 +1,61 @@
-const panel=[
-  {n:"RAM 1GB",p:2000},
-  {n:"RAM 2GB",p:4000},
-  {n:"RAM 3GB",p:6000},
-  {n:"RAM 4GB",p:8000},
-  {n:"RAM 5GB",p:10000},
-  {n:"RAM 6GB",p:12000},
-  {n:"RAM 7GB",p:14000},
-  {n:"RAM 8GB",p:16000},
-  {n:"RAM 9GB",p:18000},
-  {n:"RAM 10GB",p:20000}
+/* ================= PANEL BOT WA ================= */
+const panel = [
+  {n:"Panel Bot WA RAM 1GB (PT Roda Actli)", p:2000},
+  {n:"Panel Bot WA RAM 2GB (PT Roda Actli)", p:4000},
+  {n:"Panel Bot WA RAM 3GB (PT Roda Actli)", p:6000},
+  {n:"Panel Bot WA RAM 4GB (PT Roda Actli)", p:8000},
+  {n:"Panel Bot WA RAM 5GB (PT Roda Actli)", p:10000},
+  {n:"Panel Bot WA RAM 6GB (PT Roda Actli)", p:12000},
+  {n:"Panel Bot WA RAM 7GB (PT Roda Actli)", p:14000},
+  {n:"Panel Bot WA RAM 8GB (PT Roda Actli)", p:16000},
+  {n:"Panel Bot WA RAM 9GB (PT Roda Actli)", p:18000},
+  {n:"Panel Bot WA RAM 10GB (PT Roda Actli)", p:20000}
 ];
 
-const followers=[
-  {n:"1000 Followers",p:45000},
-  {n:"2000 Followers",p:90000},
-  {n:"3000 Followers",p:135000},
-  {n:"4000 Followers",p:180000},
-  {n:"5000 Followers",p:225000},
-  {n:"10000 Followers",p:450000}
+/* ================= FOLLOWERS IG ================= */
+const followers = [
+  {n:"Followers Instagram Indonesia 1000", p:45000},
+  {n:"Followers Instagram Indonesia 2000", p:90000},
+  {n:"Followers Instagram Indonesia 3000", p:135000},
+  {n:"Followers Instagram Indonesia 4000", p:180000},
+  {n:"Followers Instagram Indonesia 5000", p:225000},
+  {n:"Followers Instagram Indonesia 6000", p:270000},
+  {n:"Followers Instagram Indonesia 7000", p:315000},
+  {n:"Followers Instagram Indonesia 8000", p:360000},
+  {n:"Followers Instagram Indonesia 9000", p:405000},
+  {n:"Followers Instagram Indonesia 10000", p:450000}
 ];
+
+/* ================= AUTO RENDER ================= */
+const panelList = document.getElementById("panel-list");
+const followersList = document.getElementById("followers-list");
 
 /* RENDER PANEL */
-const panelList=document.getElementById("panel-list");
-panel.forEach(i=>{
-  panelList.innerHTML+=`
-  <div class="card">
-    <b>${i.n}</b><br>
-    Rp${i.p}
-    <button class="btn" onclick="addToCart('${i.n}',${i.p})">Order</button>
-  </div>`;
-});
+if(panelList){
+  panel.forEach(i=>{
+    panelList.innerHTML += `
+      <div class="product">
+        <div>
+          <b>${i.n}</b><br>
+          Rp${i.p/1000}K
+        </div>
+        <button class="btn" onclick="add('${i.n}',${i.p})">Order</button>
+      </div>
+    `;
+  });
+}
 
 /* RENDER FOLLOWERS */
-const followersList=document.getElementById("followers-list");
-followers.forEach(i=>{
-  followersList.innerHTML+=`
-  <div class="card">
-    <b>${i.n}</b><br>
-    Rp${i.p}
-    <button class="btn" onclick="addToCart('${i.n}',${i.p})">Order</button>
-  </div>`;
-});
+if(followersList){
+  followers.forEach(i=>{
+    followersList.innerHTML += `
+      <div class="product">
+        <div>
+          <b>${i.n}</b><br>
+          Rp${i.p/1000}K
+        </div>
+        <button class="btn" onclick="add('${i.n}',${i.p})">Order</button>
+      </div>
+    `;
+  });
+}
