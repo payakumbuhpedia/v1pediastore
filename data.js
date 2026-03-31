88295245dab65b21db06a4b2bb44443f754c3abb
payakumbuhpedia <payakumbuhpediastoreid@gmail.com>
2026-03-31 21:23:38 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,25 @@
+const panel = [
+  {nama:"RAM 1GB",harga:"Rp2K"},
+  {nama:"RAM 2GB",harga:"Rp4K"},
+  {nama:"RAM 3GB",harga:"Rp6K"},
+  {nama:"RAM 4GB",harga:"Rp8K"},
+  {nama:"RAM 5GB",harga:"Rp10K"},
+  {nama:"RAM 6GB",harga:"Rp12K"},
+  {nama:"RAM 7GB",harga:"Rp14K"},
+  {nama:"RAM 8GB",harga:"Rp16K"},
+  {nama:"RAM 9GB",harga:"Rp18K"},
+  {nama:"RAM 10GB",harga:"Rp20K"}
+];
+
+const followers = [
+  {nama:"1000 Followers",harga:"Rp5K"},
+  {nama:"2000 Followers",harga:"Rp10K"},
+  {nama:"3000 Followers",harga:"Rp15K"},
+  {nama:"4000 Followers",harga:"Rp20K"},
+  {nama:"5000 Followers",harga:"Rp25K"},
+  {nama:"6000 Followers",harga:"Rp30K"},
+  {nama:"7000 Followers",harga:"Rp35K"},
+  {nama:"8000 Followers",harga:"Rp40K"},
+  {nama:"9000 Followers",harga:"Rp45K"},
+  {nama:"10000 Followers",harga:"Rp50K"}
+];
