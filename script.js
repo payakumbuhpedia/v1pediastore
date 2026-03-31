087e3e97ba369c804a43ce866dad1f975e048913
payakumbuhpedia <payakumbuhpediastoreid@gmail.com>
2026-03-31 22:45:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,92 +1,76 @@
 let cart=[];
 
-/* SOUND */
-const clickSound=new Audio("https://www.soundjay.com/buttons/sounds/button-16.mp3");
-
 /* LOADING */
-window.onload=()=>{
-  setTimeout(()=>{
-    document.getElementById("loading").style.display="none";
-  },1500);
-};
-
-/* NAVIGATION */
-function showPage(id){
-  clickSound.play();
-
-  document.querySelectorAll(".page").forEach(p=>p.classList.remove("active"));
-  document.getElementById(id).classList.add("active");
-
-  document.querySelectorAll(".navbar div").forEach(n=>n.classList.remove("active-nav"));
-  event.currentTarget.classList.add("active-nav");
-}
+setTimeout(()=>{
+document.getElementById("loading").style.display="none";
+},1500);
 
 /* RENDER */
 function render(){
-  let list=document.getElementById("home-list");
-  list.innerHTML="";
+let p=document.getElementById("panel-list");
+let f=document.getElementById("followers-list");
 
-  panel.forEach(i=>{
-    list.innerHTML+=`
-    <div class="product">
-      <div>${i.nama}<br>Rp${i.harga/1000}K</div>
-      <button class="btn" onclick="add('${i.nama}',${i.harga})">Order</button>
-    </div>`;
-  });
+p.innerHTML="";
+f.innerHTML="";
 
-  followers.forEach(i=>{
-    list.innerHTML+=`
-    <div class="product">
-      <div>${i.nama}<br>Rp${i.harga/1000}K</div>
-      <button class="btn" onclick="add('${i.nama}',${i.harga})">Order</button>
-    </div>`;
-  });
+panel.forEach(i=>p.innerHTML+=html(i));
+followers.forEach(i=>f.innerHTML+=html(i));
 }
 
-/* ADD CART */
+function html(i){
+return `<div class="product">
+<div>${i.nama}<br>Rp${i.harga/1000}K</div>
+<button class="btn" onclick="add('${i.nama}',${i.harga})">Order</button>
+</div>`;
+}
+
+/* TAB */
+function switchTab(e,t){
+document.getElementById("panel-list").style.display=t=="panel"?"block":"none";
+document.getElementById("followers-list").style.display=t=="followers"?"block":"none";
+
+document.querySelectorAll(".tab").forEach(x=>x.classList.remove("active"));
+e.target.classList.add("active");
+}
+
+/* CART */
 function add(n,h){
-  clickSound.play();
-
-  cart.push({n,h});
-  updateCart();
-  popup();
+cart.push({n,h});
+updateCart();
 }
 
-/* UPDATE CART */
 function updateCart(){
-  let el=document.getElementById("cartList");
-  let total=0;
+let el=document.getElementById("cartList");
+let total=0;
+el.innerHTML="";
 
-  el.innerHTML="";
-  cart.forEach(i=>{
-    el.innerHTML+=`<div>${i.n} - ${i.h/1000}K</div>`;
-    total+=i.h;
-  });
+cart.forEach(i=>{
+el.innerHTML+=`<div>${i.n} - ${i.h/1000}K</div>`;
+total+=i.h;
+});
 
-  document.getElementById("total").innerText="Total: Rp"+total/1000+"K";
-  document.getElementById("badge").innerText=cart.length;
+document.getElementById("total").innerText="Total: Rp"+total/1000+"K";
+document.getElementById("badge").innerText=cart.length;
 }
 
-/* CLEAR CART */
 function clearCart(){
-  cart=[];
-  updateCart();
-}
-
-/* POPUP */
-function popup(){
-  let p=document.getElementById("popup");
-  p.classList.add("active");
-  setTimeout(()=>p.classList.remove("active"),1200);
+cart=[];
+updateCart();
 }
 
 /* WA */
 function orderWA(){
-  let text="🔥 ORDER V1PEDIA 🔥%0A";
-  cart.forEach(i=>text+="• "+i.n+"%0A");
+let text="ORDER V1PEDIA%0A";
+cart.forEach(i=>text+=i.n+"%0A");
 
-  window.location.href="https://wa.me/6283143490913?text="+text;
+window.location.href="https://wa.me/6283143490913?text="+text;
 }
 
-/* INIT */
+/* NAV */
+function openPage(id,el){
+document.querySelectorAll(".page").forEach(p=>p.classList.remove("active"));
+document.getElementById(id).classList.add("active");
+}
+
+/* START */
 render();
