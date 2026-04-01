aec0ddb2203c9f4c1375e6aba416d1d313cc756c
payakumbuhpedia <payakumbuhpediastoreid@gmail.com>
2026-04-01 16:33:40 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,25 +1,20 @@
 const panel = [
-  {n:"Panel Pterodactyl 1GB",h:40000},
-  {n:"Panel Pterodactyl 2GB",h:80000},
-  {n:"Panel Pterodactyl 3GB",h:90000},
-  {n:"Panel Pterodactyl 4GB",h:100000},
-  {n:"Panel Pterodactyl 5GB",h:120000},
-  {n:"Panel Pterodactyl 6GB",h:150000},
-  {n:"Panel Pterodactyl 7GB",h:180000},
-  {n:"Panel Pterodactyl 8GB",h:200000},
-  {n:"Panel Pterodactyl 9GB",h:250000},
-  {n:"Panel Pterodactyl 10GB",h:300000}
+  {n:"Panel Pterodactyl 1GB",h:2000},
+  {n:"Panel Pterodactyl 2GB",h:4000},
+  {n:"Panel Pterodactyl 3GB",h:6000},
+  {n:"Panel Pterodactyl 4GB",h:8000},
+  {n:"Panel Pterodactyl 5GB",h:10000},
+  {n:"Panel Pterodactyl 6GB",h:12000},
+  {n:"Panel Pterodactyl 7GB",h:14000},
+  {n:"Panel Pterodactyl 8GB",h:16000},
+  {n:"Panel Pterodactyl 9GB",h:18000},
+  {n:"Panel Pterodactyl 10GB",h:20000}
 ];
 
 const followers = [
-  {n:"Followers IG 1000",h:40000},
-  {n:"Followers IG 2000",h:80000},
-  {n:"Followers IG 3000",h:120000},
-  {n:"Followers IG 4000",h:160000},
-  {n:"Followers IG 5000",h:200000},
-  {n:"Followers IG 6000",h:240000},
-  {n:"Followers IG 7000",h:280000},
-  {n:"Followers IG 8000",h:320000},
-  {n:"Followers IG 9000",h:360000},
-  {n:"Followers IG 10000",h:400000}
+  {n:"Followers Instagram Indonesia 1000",h:45000},
+  {n:"Followers Instagram Indonesia 2000",h:85000},
+  {n:"Followers Instagram Indonesia 3000",h:130000},
+  {n:"Followers Instagram Indonesia 4000",h:170000},
+  {n:"Followers Instagram Indonesia 5000",h:210000}
 ];
