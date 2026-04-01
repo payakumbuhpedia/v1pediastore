678c46c8d2953fb8c3b622238c62b8173864a68d
payakumbuhpedia <payakumbuhpediastoreid@gmail.com>
2026-04-01 19:26:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,134 +1,90 @@
-let cart = [];
+let cart=[];
 
-/* 🔊 SOUND SYSTEM (ULTRA FIX HP) */
-let soundReady = false;
-let clickSound;
+/* LOADING */
+window.onload=()=>{
+  setTimeout(()=>{
+    document.getElementById("loading").style.display="none";
+  },1000);
+};
 
-/* UNLOCK SOUND */
-document.body.addEventListener("click", function init(){
-  clickSound = new Audio("https://assets.mixkit.co/sfx/preview/mixkit-select-click-1109.mp3");
-  soundReady = true;
-  document.body.removeEventListener("click", init);
-});
-
-/* PLAY SOUND */
-function sfx(){
-  if(soundReady){
-    clickSound.currentTime = 0;
-    clickSound.play();
-  }
+function sound(){
+  document.getElementById("clickSound").play();
+  if(navigator.vibrate) navigator.vibrate(50);
 }
 
-/* GETAR */
-function vibrate(){
-  if(navigator.vibrate){
-    navigator.vibrate(30);
-  }
-}
-
-/* EFFECT KLIK PREMIUM */
-function fx(el){
-  sfx();
-  vibrate();
-
-  if(el){
-    el.classList.add("tap");
-    setTimeout(()=>el.classList.remove("tap"),150);
-  }
-}
-
-/* 🔥 NAV FIX */
 function nav(id){
-  fx();
-
-  document.querySelectorAll(".page").forEach(p=>{
-    p.classList.remove("active");
-  });
-
+  sound();
+  document.querySelectorAll(".page").forEach(p=>p.classList.remove("active"));
   document.getElementById(id).classList.add("active");
 }
 
 /* FORMAT HARGA */
 function format(h){
-  return (h>=1000) ? "Rp"+(h/1000)+"K" : "Rp"+h;
+  return h>=1000 ? (h/1000)+"K" : h;
 }
 
 /* PANEL */
 function openPanel(el){
-  fx(el);
-
-  let list = document.getElementById("panel-list");
+  sound();
+  let list=document.getElementById("panel-list");
   list.innerHTML="";
-  document.getElementById("followers-list").innerHTML="";
 
   panel.forEach(p=>{
-    list.innerHTML += `
+    list.innerHTML+=`
     <div class="product">
-      <div>${p.n}<br>${format(p.h)}</div>
-      <button onclick="add('${p.n}',${p.h},this)">Order</button>
+      ${p.nama}<br>Rp${format(p.harga)}
+      <button onclick="addCart('${p.nama}',${p.harga})">Order</button>
     </div>`;
   });
 }
 
 /* FOLLOWERS */
-function openFollowers(el){
-  fx(el);
-
-  let list = document.getElementById("followers-list");
+function openFollowers(){
+  sound();
+  let list=document.getElementById("followers-list");
   list.innerHTML="";
-  document.getElementById("panel-list").innerHTML="";
 
   followers.forEach(f=>{
-    list.innerHTML += `
+    list.innerHTML+=`
     <div class="product">
-      <div>${f.n}<br>${format(f.h)}</div>
-      <button onclick="add('${f.n}',${f.h},this)">Order</button>
+      ${f.nama}<br>Rp${format(f.harga)}
+      <button onclick="addCart('${f.nama}',${f.harga})">Order</button>
     </div>`;
   });
 }
 
-/* ADD CART */
-function add(n,h,el){
-  fx(el);
-
-  cart.push({n,h});
+/* CART */
+function addCart(nama,harga){
+  sound();
+  cart.push({nama,harga});
   renderCart();
 }
 
-/* RENDER CART */
 function renderCart(){
-  let el = document.getElementById("cartList");
-  let total = 0;
-  el.innerHTML="";
+  let list=document.getElementById("cartList");
+  let total=0;
+  list.innerHTML="";
 
-  cart.forEach(c=>{
-    total += c.h;
-
-    el.innerHTML += `
+  cart.forEach(i=>{
+    total+=i.harga;
+    list.innerHTML+=`
     <div class="product">
-      ${c.n} - ${format(c.h)}
+      ${i.nama} - Rp${format(i.harga)}
     </div>`;
   });
 
-  document.getElementById("total").innerText = "Total: "+format(total);
+  document.getElementById("total").innerText="Total: Rp"+format(total);
 }
 
-/* CLEAR */
 function clearCart(){
-  fx();
+  sound();
   cart=[];
   renderCart();
 }
 
-/* WA */
 function orderWA(){
-  fx();
-
-  let text="Halo admin, saya ingin order:%0A";
-
-  cart.forEach(c=>{
-    text += `- ${c.n} (${format(c.h)})%0A`;
-  });
-
-  window.open("https://wa.me/6283143490913?text="+text);
+  sound();
+  let text="Order:\n";
+  cart.forEach(i=> text+=i.nama+"\n");
+  window.open("https://wa.me/6283143490913?text="+encodeURIComponent(text));
 }
