721d51b92836d1ead8359d60912cdda454fad047
payakumbuhpedia <payakumbuhpediastoreid@gmail.com>
2026-04-01 22:16:53 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,20 +1,43 @@
-const panel = [
-  {n:"Panel Pterodactyl 1GB",h:2000},
-  {n:"Panel Pterodactyl 2GB",h:4000},
-  {n:"Panel Pterodactyl 3GB",h:6000},
-  {n:"Panel Pterodactyl 4GB",h:8000},
-  {n:"Panel Pterodactyl 5GB",h:10000},
-  {n:"Panel Pterodactyl 6GB",h:12000},
-  {n:"Panel Pterodactyl 7GB",h:14000},
-  {n:"Panel Pterodactyl 8GB",h:16000},
-  {n:"Panel Pterodactyl 9GB",h:18000},
-  {n:"Panel Pterodactyl 10GB",h:20000}
-];
+const products=[
 
-const followers = [
-  {n:"Followers Instagram Indonesia 1000",h:45000},
-  {n:"Followers Instagram Indonesia 2000",h:85000},
-  {n:"Followers Instagram Indonesia 3000",h:130000},
-  {n:"Followers Instagram Indonesia 4000",h:170000},
-  {n:"Followers Instagram Indonesia 5000",h:210000}
+// IG Followers
+{kategori:"Followers Instagram",nama:"1K Followers IG",harga:45000},
+{kategori:"Followers Instagram",nama:"2K Followers IG",harga:90000},
+{kategori:"Followers Instagram",nama:"3K Followers IG",harga:135000},
+{kategori:"Followers Instagram",nama:"4K Followers IG",harga:180000},
+{kategori:"Followers Instagram",nama:"5K Followers IG",harga:225000},
+
+// IG Like
+{kategori:"Like Instagram",nama:"1K Like IG",harga:30000},
+{kategori:"Like Instagram",nama:"2K Like IG",harga:60000},
+{kategori:"Like Instagram",nama:"3K Like IG",harga:90000},
+{kategori:"Like Instagram",nama:"4K Like IG",harga:120000},
+{kategori:"Like Instagram",nama:"5K Like IG",harga:150000},
+
+// TikTok Followers
+{kategori:"Followers TikTok",nama:"1K Followers TikTok",harga:50000},
+{kategori:"Followers TikTok",nama:"2K Followers TikTok",harga:100000},
+{kategori:"Followers TikTok",nama:"3K Followers TikTok",harga:150000},
+{kategori:"Followers TikTok",nama:"4K Followers TikTok",harga:200000},
+{kategori:"Followers TikTok",nama:"5K Followers TikTok",harga:250000},
+
+// TikTok Like
+{kategori:"Like TikTok",nama:"1K Like TikTok",harga:45000},
+{kategori:"Like TikTok",nama:"2K Like TikTok",harga:90000},
+{kategori:"Like TikTok",nama:"3K Like TikTok",harga:135000},
+{kategori:"Like TikTok",nama:"4K Like TikTok",harga:180000},
+{kategori:"Like TikTok",nama:"5K Like TikTok",harga:225000},
+
+// Panel
+{kategori:"Panel Pterodactyl",nama:"Panel 1GB",harga:3000},
+{kategori:"Panel Pterodactyl",nama:"Panel 2GB",harga:7000},
+{kategori:"Panel Pterodactyl",nama:"Panel 3GB",harga:9000},
+{kategori:"Panel Pterodactyl",nama:"Panel 4GB",harga:11000},
+{kategori:"Panel Pterodactyl",nama:"Panel 5GB",harga:13000},
+{kategori:"Panel Pterodactyl",nama:"Panel 6GB",harga:15000},
+{kategori:"Panel Pterodactyl",nama:"Panel 7GB",harga:17000},
+{kategori:"Panel Pterodactyl",nama:"Panel 8GB",harga:19000},
+{kategori:"Panel Pterodactyl",nama:"Panel 9GB",harga:21000},
+{kategori:"Panel Pterodactyl",nama:"Panel 10GB",harga:23000}
+
 ];
