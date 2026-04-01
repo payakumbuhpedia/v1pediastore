ea3a27d737dd40c452c43d738f0f63f8306e2e58
payakumbuhpedia <payakumbuhpediastoreid@gmail.com>
2026-04-01 12:39:55 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,25 +1,25 @@
-const panel = [
-  {n:"Panel Pterodactyl 1GB",h:2000},
-  {n:"Panel Pterodactyl 2GB",h:4000},
-  {n:"Panel Pterodactyl 3GB",h:6000},
-  {n:"Panel Pterodactyl 4GB",h:8000},
-  {n:"Panel Pterodactyl 5GB",h:10000},
-  {n:"Panel Pterodactyl 6GB",h:12000},
-  {n:"Panel Pterodactyl 7GB",h:14000},
-  {n:"Panel Pterodactyl 8GB",h:16000},
-  {n:"Panel Pterodactyl 9GB",h:18000},
-  {n:"Panel Pterodactyl 10GB",h:20000}
+const panelData = [
+  { nama:"Panel Pterodactyl 1GB", harga:2000 },
+  { nama:"Panel Pterodactyl 2GB", harga:4000 },
+  { nama:"Panel Pterodactyl 3GB", harga:6000 },
+  { nama:"Panel Pterodactyl 4GB", harga:8000 },
+  { nama:"Panel Pterodactyl 5GB", harga:10000 },
+  { nama:"Panel Pterodactyl 6GB", harga:12000 },
+  { nama:"Panel Pterodactyl 7GB", harga:14000 },
+  { nama:"Panel Pterodactyl 8GB", harga:16000 },
+  { nama:"Panel Pterodactyl 9GB", harga:18000 },
+  { nama:"Panel Pterodactyl 10GB", harga:20000 }
 ];
 
-const followers = [
-  {n:"Followers Instagram Indonesia 1000",h:45000},
-  {n:"Followers Instagram Indonesia 2000",h:85000},
-  {n:"Followers Instagram Indonesia 3000",h:130000},
-  {n:"Followers Instagram Indonesia 4000",h:170000},
-  {n:"Followers Instagram Indonesia 5000",h:210000},
-  {n:"Followers Instagram Indonesia 6000",h:250000},
-  {n:"Followers Instagram Indonesia 7000",h:290000},
-  {n:"Followers Instagram Indonesia 8000",h:330000},
-  {n:"Followers Instagram Indonesia 9000",h:370000},
-  {n:"Followers Instagram Indonesia 10000",h:400000}
+const followersData = [
+  { nama:"Followers Instagram Indonesia 1000", harga:45000 },
+  { nama:"Followers Instagram Indonesia 2000", harga:85000 },
+  { nama:"Followers Instagram Indonesia 3000", harga:130000 },
+  { nama:"Followers Instagram Indonesia 4000", harga:170000 },
+  { nama:"Followers Instagram Indonesia 5000", harga:210000 },
+  { nama:"Followers Instagram Indonesia 6000", harga:250000 },
+  { nama:"Followers Instagram Indonesia 7000", harga:290000 },
+  { nama:"Followers Instagram Indonesia 8000", harga:330000 },
+  { nama:"Followers Instagram Indonesia 9000", harga:370000 },
+  { nama:"Followers Instagram Indonesia 10000", harga:400000 }
 ];
