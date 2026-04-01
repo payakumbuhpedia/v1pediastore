af01f27c610c9d155918312eb62a046813a0a400
payakumbuhpedia <payakumbuhpediastoreid@gmail.com>
2026-04-01 22:16:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,90 +1,83 @@
 let cart=[];
 
 /* LOADING */
-window.onload=()=>{
-  setTimeout(()=>{
-    document.getElementById("loading").style.display="none";
-  },1000);
-};
+window.onload=()=>setTimeout(()=>loading.style.display="none",1000);
 
-function sound(){
-  document.getElementById("clickSound").play();
-  if(navigator.vibrate) navigator.vibrate(50);
+/* SOUND */
+function sfx(){
+let s=document.getElementById("sfx");
+s.currentTime=0;
+s.play();
+if(navigator.vibrate) navigator.vibrate(50);
 }
 
+/* NAV */
 function nav(id){
-  sound();
-  document.querySelectorAll(".page").forEach(p=>p.classList.remove("active"));
-  document.getElementById(id).classList.add("active");
+sfx();
+document.querySelectorAll(".page").forEach(p=>p.classList.remove("active"));
+document.getElementById(id).classList.add("active");
 }
 
-/* FORMAT HARGA */
+/* FORMAT */
 function format(h){
-  return h>=1000 ? (h/1000)+"K" : h;
+return h>=1000?(h/1000)+"K":h;
 }
 
-/* PANEL */
-function openPanel(el){
-  sound();
-  let list=document.getElementById("panel-list");
-  list.innerHTML="";
+/* CATEGORY */
+function openCategory(cat){
+sfx();
+let list=document.getElementById("productList");
+list.innerHTML="";
 
-  panel.forEach(p=>{
-    list.innerHTML+=`
-    <div class="product">
-      ${p.nama}<br>Rp${format(p.harga)}
-      <button onclick="addCart('${p.nama}',${p.harga})">Order</button>
-    </div>`;
-  });
-}
-
-/* FOLLOWERS */
-function openFollowers(){
-  sound();
-  let list=document.getElementById("followers-list");
-  list.innerHTML="";
-
-  followers.forEach(f=>{
-    list.innerHTML+=`
-    <div class="product">
-      ${f.nama}<br>Rp${format(f.harga)}
-      <button onclick="addCart('${f.nama}',${f.harga})">Order</button>
-    </div>`;
-  });
+products.filter(p=>p.kategori===cat).forEach(p=>{
+list.innerHTML+=`
+<div class="product">
+${p.nama}<br>Rp${format(p.harga)}
+<button onclick="addCart('${p.nama}',${p.harga})">Order</button>
+</div>`;
+});
 }
 
 /* CART */
-function addCart(nama,harga){
-  sound();
-  cart.push({nama,harga});
-  renderCart();
+function addCart(n,h){
+sfx();
+cart.push({n,h});
+renderCart();
 }
 
 function renderCart(){
-  let list=document.getElementById("cartList");
-  let total=0;
-  list.innerHTML="";
+let list=document.getElementById("cartList");
+let total=0;
+list.innerHTML="";
 
-  cart.forEach(i=>{
-    total+=i.harga;
-    list.innerHTML+=`
-    <div class="product">
-      ${i.nama} - Rp${format(i.harga)}
-    </div>`;
-  });
+cart.forEach(i=>{
+total+=i.h;
+list.innerHTML+=`<div class="product">${i.n} - Rp${format(i.h)}</div>`;
+});
 
-  document.getElementById("total").innerText="Total: Rp"+format(total);
+total.innerText="Total: Rp"+format(total);
 }
 
+/* CLEAR */
 function clearCart(){
-  sound();
-  cart=[];
-  renderCart();
+sfx();
+cart=[];
+renderCart();
 }
 
+/* WA */
 function orderWA(){
-  sound();
-  let text="Order:\n";
-  cart.forEach(i=> text+=i.nama+"\n");
-  window.open("https://wa.me/6283143490913?text="+encodeURIComponent(text));
+sfx();
+let kode=Math.floor(Math.random()*999999);
+
+let text=`Halo V1PEDIASTORE 👋
+
+Order:
+`;
+
+cart.forEach(i=>text+=`- ${i.n}\n`);
+
+text+=`\nKode: V1-${kode}`;
+
+window.open("https://wa.me/6283143490913?text="+encodeURIComponent(text));
 }
