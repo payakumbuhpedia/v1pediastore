d1d11e75511b08ac09a0563178c2ce76a0942a5d
payakumbuhpedia <payakumbuhpediastoreid@gmail.com>
2026-04-01 15:38:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 let cart = [];
 
+/* 🔊 SOUND SYSTEM */
+const sClick = new Audio("https://assets.mixkit.co/sfx/preview/mixkit-select-click-1109.mp3");
+const sOpen = new Audio("https://assets.mixkit.co/sfx/preview/mixkit-ui-click-1119.mp3");
+const sAdd = new Audio("https://assets.mixkit.co/sfx/preview/mixkit-positive-interface-beep-221.mp3");
+
+/* NAV */
 function nav(id){
+  sClick.play();
+
   document.querySelectorAll('.page').forEach(p=>{
     p.classList.remove('active');
   });
   document.getElementById(id).classList.add('active');
 }
 
+/* PANEL */
 function openPanel(){
+  sOpen.play();
+
   let list = document.getElementById('panel-list');
   list.innerHTML = "";
+  document.getElementById('followers-list').innerHTML = "";
+
   panel.forEach(p=>{
     list.innerHTML += `
       <div class="product">
@@ -20,9 +33,14 @@ function openPanel(){
   });
 }
 
+/* FOLLOWERS */
 function openFollowers(){
+  sOpen.play();
+
   let list = document.getElementById('followers-list');
   list.innerHTML = "";
+  document.getElementById('panel-list').innerHTML = "";
+
   followers.forEach(f=>{
     list.innerHTML += `
       <div class="product">
@@ -33,12 +51,16 @@ function openFollowers(){
   });
 }
 
+/* ADD CART */
 function add(n,h){
+  sAdd.play();
+
   cart.push({n,h});
   renderCart();
   nav('cart');
 }
 
+/* RENDER CART */
 function renderCart(){
   let el = document.getElementById('cartList');
   let total = 0;
@@ -52,12 +74,18 @@ function renderCart(){
   document.getElementById('total').innerText = "Total: Rp"+total;
 }
 
+/* CLEAR CART */
 function clearCart(){
+  sClick.play();
+
   cart=[];
   renderCart();
 }
 
+/* ORDER WA */
 function orderWA(){
+  sOpen.play();
+
   let text="Halo Admin, saya ingin order:%0A";
   cart.forEach(c=>{
     text += `- ${c.n} (Rp${c.h})%0A`;
