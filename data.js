41fa2ee52fc0288708c2aaafd22b79fe23fae9f8
payakumbuhpedia <payakumbuhpediastoreid@gmail.com>
2026-04-01 12:17:03 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,25 +1,25 @@
 const panelData = [
-  {nama:"Panel Bot 1GB", harga:2000},
-  {nama:"Panel Bot 2GB", harga:4000},
-  {nama:"Panel Bot 3GB", harga:6000},
-  {nama:"Panel Bot 4GB", harga:8000},
-  {nama:"Panel Bot 5GB", harga:10000},
-  {nama:"Panel Bot 6GB", harga:12000},
-  {nama:"Panel Bot 7GB", harga:14000},
-  {nama:"Panel Bot 8GB", harga:16000},
-  {nama:"Panel Bot 9GB", harga:18000},
-  {nama:"Panel Bot 10GB", harga:20000}
+  {nama:"Panel Bot WhatsApp RAM 1GB", harga:2000},
+  {nama:"Panel Bot WhatsApp RAM 2GB", harga:4000},
+  {nama:"Panel Bot WhatsApp RAM 3GB", harga:6000},
+  {nama:"Panel Bot WhatsApp RAM 4GB", harga:8000},
+  {nama:"Panel Bot WhatsApp RAM 5GB", harga:10000},
+  {nama:"Panel Bot WhatsApp RAM 6GB", harga:12000},
+  {nama:"Panel Bot WhatsApp RAM 7GB", harga:14000},
+  {nama:"Panel Bot WhatsApp RAM 8GB", harga:16000},
+  {nama:"Panel Bot WhatsApp RAM 9GB", harga:18000},
+  {nama:"Panel Bot WhatsApp RAM 10GB", harga:20000}
 ];
 
 const followersData = [
-  {nama:"1000 Followers", harga:5000},
-  {nama:"2000 Followers", harga:9000},
-  {nama:"3000 Followers", harga:13000},
-  {nama:"4000 Followers", harga:17000},
-  {nama:"5000 Followers", harga:20000},
-  {nama:"6000 Followers", harga:23000},
-  {nama:"7000 Followers", harga:26000},
-  {nama:"8000 Followers", harga:29000},
-  {nama:"9000 Followers", harga:32000},
-  {nama:"10000 Followers", harga:35000}
+  {nama:"1000 Followers Instagram", harga:5000},
+  {nama:"2000 Followers Instagram", harga:9000},
+  {nama:"3000 Followers Instagram", harga:13000},
+  {nama:"4000 Followers Instagram", harga:17000},
+  {nama:"5000 Followers Instagram", harga:20000},
+  {nama:"6000 Followers Instagram", harga:23000},
+  {nama:"7000 Followers Instagram", harga:26000},
+  {nama:"8000 Followers Instagram", harga:29000},
+  {nama:"9000 Followers Instagram", harga:32000},
+  {nama:"10000 Followers Instagram", harga:35000}
 ];
