80ff8886436d58530dfed65048da944459735a7d
payakumbuhpedia <payakumbuhpediastoreid@gmail.com>
2026-04-01 12:29:11 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,25 +1,28 @@
-const panelData = [
-  {nama:"Panel Bot WhatsApp RAM 1GB", harga:2000},
-  {nama:"Panel Bot WhatsApp RAM 2GB", harga:4000},
-  {nama:"Panel Bot WhatsApp RAM 3GB", harga:6000},
-  {nama:"Panel Bot WhatsApp RAM 4GB", harga:8000},
-  {nama:"Panel Bot WhatsApp RAM 5GB", harga:10000},
-  {nama:"Panel Bot WhatsApp RAM 6GB", harga:12000},
-  {nama:"Panel Bot WhatsApp RAM 7GB", harga:14000},
-  {nama:"Panel Bot WhatsApp RAM 8GB", harga:16000},
-  {nama:"Panel Bot WhatsApp RAM 9GB", harga:18000},
-  {nama:"Panel Bot WhatsApp RAM 10GB", harga:20000}
+// ================= PANEL BOT WHATSAPP =================
+const panel = [
+  {n:"Panel Bot WhatsApp RAM 1GB (Pterodactyl)", p:2000},
+  {n:"Panel Bot WhatsApp RAM 2GB (Pterodactyl)", p:4000},
+  {n:"Panel Bot WhatsApp RAM 3GB (Pterodactyl)", p:6000},
+  {n:"Panel Bot WhatsApp RAM 4GB (Pterodactyl)", p:8000},
+  {n:"Panel Bot WhatsApp RAM 5GB (Pterodactyl)", p:10000},
+  {n:"Panel Bot WhatsApp RAM 6GB (Pterodactyl)", p:12000},
+  {n:"Panel Bot WhatsApp RAM 7GB (Pterodactyl)", p:14000},
+  {n:"Panel Bot WhatsApp RAM 8GB (Pterodactyl)", p:16000},
+  {n:"Panel Bot WhatsApp RAM 9GB (Pterodactyl)", p:18000},
+  {n:"Panel Bot WhatsApp RAM 10GB (Pterodactyl)", p:20000}
 ];
 
-const followersData = [
-  {nama:"1000 Followers Instagram", harga:5000},
-  {nama:"2000 Followers Instagram", harga:9000},
-  {nama:"3000 Followers Instagram", harga:13000},
-  {nama:"4000 Followers Instagram", harga:17000},
-  {nama:"5000 Followers Instagram", harga:20000},
-  {nama:"6000 Followers Instagram", harga:23000},
-  {nama:"7000 Followers Instagram", harga:26000},
-  {nama:"8000 Followers Instagram", harga:29000},
-  {nama:"9000 Followers Instagram", harga:32000},
-  {nama:"10000 Followers Instagram", harga:35000}
+
+// ================= FOLLOWERS INSTAGRAM =================
+const followers = [
+  {n:"Followers Instagram Indonesia 1000 (Real User)", p:45000},
+  {n:"Followers Instagram Indonesia 2000 (Real User)", p:85000},
+  {n:"Followers Instagram Indonesia 3000 (Real User)", p:130000},
+  {n:"Followers Instagram Indonesia 4000 (Real User)", p:170000},
+  {n:"Followers Instagram Indonesia 5000 (Real User)", p:210000},
+  {n:"Followers Instagram Indonesia 6000 (Real User)", p:250000},
+  {n:"Followers Instagram Indonesia 7000 (Real User)", p:290000},
+  {n:"Followers Instagram Indonesia 8000 (Real User)", p:330000},
+  {n:"Followers Instagram Indonesia 9000 (Real User)", p:370000},
+  {n:"Followers Instagram Indonesia 10000 (Real User)", p:400000}
 ];
