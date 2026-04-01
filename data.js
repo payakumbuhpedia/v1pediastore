6c3e7c2d80a4f8a1ba6fe4c06ae892475f28137f
payakumbuhpedia <payakumbuhpediastoreid@gmail.com>
2026-04-01 08:57:29 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,16 +1,25 @@
-const panel = [
-  {n:"RAM 1GB Panel Pterodactyl",p:2000},
-  {n:"RAM 2GB Panel Pterodactyl",p:4000},
-  {n:"RAM 3GB Panel Pterodactyl",p:6000},
-  {n:"RAM 4GB Panel Pterodactyl",p:8000},
-  {n:"RAM 5GB Panel Pterodactyl",p:10000},
-  {n:"RAM 6GB Panel Pterodactyl",p:12000},
-  {n:"RAM 7GB Panel Pterodactyl",p:14000}
+const panel=[
+{n:"Panel Pterodactyl 1GB",p:2000},
+{n:"Panel Pterodactyl 2GB",p:4000},
+{n:"Panel Pterodactyl 3GB",p:6000},
+{n:"Panel Pterodactyl 4GB",p:8000},
+{n:"Panel Pterodactyl 5GB",p:10000},
+{n:"Panel Pterodactyl 6GB",p:12000},
+{n:"Panel Pterodactyl 7GB",p:14000},
+{n:"Panel Pterodactyl 8GB",p:16000},
+{n:"Panel Pterodactyl 9GB",p:18000},
+{n:"Panel Pterodactyl 10GB",p:20000}
 ];
 
-const followers = [
-  {n:"1000 Followers Indonesia",p:45000},
-  {n:"2000 Followers Indonesia",p:90000},
-  {n:"3000 Followers Indonesia",p:135000},
-  {n:"5000 Followers Indonesia",p:225000}
+const followers=[
+{n:"Followers IG 1000",p:45000},
+{n:"Followers IG 2000",p:90000},
+{n:"Followers IG 3000",p:135000},
+{n:"Followers IG 4000",p:180000},
+{n:"Followers IG 5000",p:225000},
+{n:"Followers IG 6000",p:270000},
+{n:"Followers IG 7000",p:315000},
+{n:"Followers IG 8000",p:360000},
+{n:"Followers IG 9000",p:405000},
+{n:"Followers IG 10000",p:450000}
 ];
